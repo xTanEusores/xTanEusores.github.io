0077f9cf62894e1c1ea186cf2b09dae9fdd88874
xTanEusores <44177748+xTanEusores@users.noreply.github.com>
2024-09-25 20:02:24 +0800

    My portfolio

---
+++ script.js
@@ -0,0 +1,9 @@
+function hamburg() {
+  const dropdown = document.getElementById('dropdown-menu');
+  dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
+}
+
+function cancel() {
+  const dropdown = document.getElementById('dropdown-menu');
+  dropdown.style.display = 'none';
+}
