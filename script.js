9578c9453b12d13d8ded63bcd0f670d527fa9996
xTanEusores <44177748+xTanEusores@users.noreply.github.com>
2024-09-25 21:57:10 +0800

    Update
    edit some text add my project section

--- script.js
+++ script.js
@@ -7,3 +7,36 @@ function cancel() {
   const dropdown = document.getElementById('dropdown-menu');
   dropdown.style.display = 'none';
 }
+
+
+const texts = ["ASPIRING WEB DEVELOPER", "BPO EMPLOYEE"];
+
+let speed = 100;
+const textElements = document.querySelector(".typewriter-text");
+let textIndex = 0;
+let charIndex = 0;
+
+function typewriter() {
+  if (charIndex < texts[textIndex].length) {
+    textElements.innerHTML += texts[textIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(typewriter, speed)
+  }
+  else {
+    setTimeout(eraseText, 1000);
+  }
+}
+
+function eraseText(){
+  if (textElements.innerHTML.length>0) {
+    textElements.innerHTML = textElements.innerHTML.slice(0,-1);
+    setTimeout(eraseText, 50);
+  }
+  else {
+    textIndex = (textIndex +1) % texts.length;
+    charIndex = 0;
+    setTimeout(typewriter,500);
+  }
+}
+
+window.onload=typewriter;
